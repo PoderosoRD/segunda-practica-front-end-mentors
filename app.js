3b4c5d103692424083e9a024593d75ee8f995cda
Orlando <paulinorpk@gmail.com>
2024-09-24 16:00:58 -0400

    Modificacion del HTML y CSS, creacion del JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+let dias = document.getElementsByClassName("dias").value;
+let meses = document.getElementsByClassName("meses").value;
+let años = document.getElementsByClassName("años").value;
 
+function cantidad_años_dias_meses() {
+     
+}
